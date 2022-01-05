110a5949c8b1770466cda05f776a8d2bb19fcb37
craig <craig@codercraig.com>
2022-01-05 23:31:14 +0000

    Adding in DELETE/UPDATE functionality to API

--- main.py
+++ main.py
@@ -61,11 +61,24 @@ def get_post(id: int, response: Response):
     return {"post_detail": post}
 
 @app.delete("/posts/{id}")
-def delete_post():
+def delete_post(id: int):
     # delete post
     # find the index in the array that has a required ID
     # my_posts()!!
     index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist!")
 
     my_posts.pop(index)
-    return {'message': "post was successfully deleted!"}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist!")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data': post_dict}
